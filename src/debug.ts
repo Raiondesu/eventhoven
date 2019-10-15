66e9d5891e678b83e280ace7e2468c170f778ed9
Raiondesu <a.raiondesu@gmail.com>
2019-10-15 14:59:45 +0300

    Refactor src/debug

--- src/debug.ts
+++ src/debug.ts
@@ -1,23 +1,23 @@
 import { on } from './subscribe';
 import { off } from './unsubscribe';
-import { metaEventMap, TMetaHandlerMap } from './meta-events';
+import { metaEventMap } from './meta-events';
+import { TEventMap, TEventHandler } from './events';
 
-const onMeta = on(metaEventMap);
-const offMeta = off(metaEventMap);
+const onMeta = on(metaEventMap)('*');
+const offMeta = off(metaEventMap)('*');
 
-const handler: TMetaHandlerMap[keyof TMetaHandlerMap] = (
-  map, event, argsOrHandler
-) => console.log(`${
+const log = (
+  map: TEventMap,
+  event: keyof TEventMap,
+  argsOrHandler: any[] | TEventHandler
+) => console.log(
+  `${
     new Date().toTimeString()
   } [EVENT "${event}"]: ${
       argsOrHandler
   } from ${map}`
 );
 
-export const debug = (enable: boolean) => {
-  if (enable) {
-    onMeta('*')(handler);
-  } else {
-    offMeta('*')(handler)
-  }
-}
+export const debug = (enable: boolean) => (
+  enable ? onMeta : offMeta
+)(log);
