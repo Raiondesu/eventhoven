251cdb4848a7302823df376078ac335e43b41dc4
Raiondesu <a.raiondesu@gmail.com>
2019-10-25 11:51:36 +0300

    Feature [harmonicWait] src/wait
     - `wait` with the same signature as other event-handling functions (emit, on, off...)

--- src/wait.ts
+++ src/wait.ts
@@ -2,7 +2,7 @@ import { TEventMap, THandlerOf } from './events.js';
 import { subscribe } from './subscribe.js';
 
 /**
- * Create event waiter for an event-map
+ * Creates an event waiter for an event-map
  *
  * @param eventMap - an event-map to create a waiter for
  * @returns an event waiter
@@ -25,3 +25,31 @@ export const wait = <M extends TEventMap>(
     (...args: Parameters<THandlerOf<M, E>>) => resolve(args)
   ) as THandlerOf<M, E>));
 };
+
+/**
+ * Creates an event waiter factory for an event-map
+ *
+ * Same as `wait`, but in-harmony with the other methods
+ * due to having an arity of 3.
+ *
+ * @param eventMap - an event-map to create a waiter for
+ * @returns an event waiter factory
+ */
+export const harmonicWait = <M extends TEventMap>(
+  eventMap: M
+) =>
+/**
+ * Wait for event to be emitted
+ *
+ * @param event - an event to wait for
+ * @returns an event waiter that returns a promise
+ */
+<E extends keyof M>(
+  event: E
+) =>
+/**
+ * Wait for event to be emitted
+ *
+ * @returns a promise that resolves as soon as the described event is emitted
+ */
+() => wait(eventMap)(event);
