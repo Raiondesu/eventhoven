3bd29304486cad2241b1aebfd9ebf2309bf5d905
Raiondesu <a.raiondesu@gmail.com>
2019-10-26 23:37:06 +0300

    Add tests/debug

---
+++ tests/debug.test.ts
@@ -0,0 +1,53 @@
+import { test_eventMap } from './common';
+import { debug, log } from '../src/debug';
+import { emit } from '../src/emit';
+import { metaEvents } from '../src/meta-events';
+import { on } from '../src/subscribe';
+
+describe('debug', () => {
+  it('toggles logs, outputs to console', async () => {
+    const originalLog = console.log;
+    const logMock = jest.fn();
+    const expectedLogs = 3;
+    const callEvents = async () => {
+      await emit(test_eventMap)('event3')();
+      await emit(test_eventMap)('event2')(false);
+      await emit(test_eventMap)('event1')('asd', 0);
+    };
+
+    console.log = logMock;
+
+    debug({ enable: true });
+
+    await callEvents();
+
+    debug({ enable: false });
+
+    await callEvents();
+
+    expect(logMock.mock.calls.length).toBe(expectedLogs);
+
+    console.log = originalLog;
+
+    expect(console.log).toBe(originalLog);
+  });
+
+  it('doesn\'t fail on invalid handler', async () => {
+    let failed = false;
+
+    try {
+      on(test_eventMap)('event3')(
+        // Intentional invalid handler
+        undefined as any
+      );
+
+      debug({ enable: true });
+
+      await emit(test_eventMap)('event3')();
+    } catch (e) {
+      failed = true;
+    }
+
+    expect(failed).toBe(false);
+  });
+});
