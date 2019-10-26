bc3b4bd0aa9bcc183b00bb0d6e4a5840c1ec6cf0
Raiondesu <a.raiondesu@gmail.com>
2019-10-26 21:03:17 +0300

    Add tests/subscribe - basic implementation

---
+++ tests/subscribe.test.ts
@@ -0,0 +1,28 @@
+import { on } from '../src/subscribe';
+import { test_eventMap } from './common';
+
+describe('subscribe', () => {
+  it('adds handler to the event pool', () => {
+    const handler = jest.fn();
+    const event = 'event3';
+
+    const cleanup = on(test_eventMap)(event)(handler);
+
+    // expect handler to be there
+    expect(
+      test_eventMap[event].handlers.has(handler)
+    ).toBe(true);
+
+    // expect handler to not be once
+    expect(
+      test_eventMap[event].handlers.get(handler)
+    ).toBe(false);
+
+    cleanup();
+
+    // expect handler to be there
+    expect(
+      test_eventMap[event].handlers.has(handler)
+    ).toBe(false);
+  });
+});
