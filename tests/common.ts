518ca99f76254022907d1cf1615bb3fa9f0ba8ba
Raiondesu <a.raiondesu@gmail.com>
2019-10-26 17:15:04 +0300

    Add [awaiter test] tests/emit - test awaiting handlers

--- tests/common.ts
+++ tests/common.ts
@@ -1,9 +1,16 @@
 import { eventMap } from '../src/events';
 
+export const test_promiseDelayMS = 1000;
+export const msInSec = 1000;
+export const test_promiseDelaySEC = test_promiseDelayMS / msInSec;
+
 export const test_eventSignatures = {
   event1(arg1: string, arg2: number) {},
   event2(arg: boolean) {},
   event3() {},
+  promisedEvent() {
+    return new Promise<void>((r) => setTimeout(r, test_promiseDelayMS));
+  },
 };
 
 export const test_eventMap = eventMap(test_eventSignatures);
--- tests/emit.test.ts
+++ tests/emit.test.ts
@@ -1,7 +1,7 @@
 import cloneDeep from 'lodash.clonedeep';
 
 import { emit } from '../src/emit';
-import { test_eventMap } from './common';
+import { test_eventMap, test_promiseDelaySEC, msInSec } from './common';
 
 
 describe('emit', () => {
@@ -11,5 +11,20 @@ describe('emit', () => {
     emit(test_eventMap)('event3')();
 
     expect(test_eventMap).toStrictEqual(prevEventMap);
+  });
+
+  it(`awaits the handlers correctly`, async () => {
+    /**
+     * Gets current time in seconds
+     */
+    const getCurrentSeconds = () => new Date().getTime() / msInSec;
+
+    const timeBefore = getCurrentSeconds();
+
+    await emit(test_eventMap)('promisedEvent')();
+
+    const timeAfter = getCurrentSeconds();
+
+    expect(timeAfter).toBeCloseTo(timeBefore + test_promiseDelaySEC, 1);
   });
 });
