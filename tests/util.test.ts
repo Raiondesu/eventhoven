26f237f2ed25358fcfe394293966629151c1bbf5
Raiondesu <a.raiondesu@gmail.com>
2019-10-26 12:41:44 +0300

    Fix [jest config] package.json, tsconfig.json

---
+++ tests/util.test.ts
@@ -0,0 +1,23 @@
+import { mapObject } from '../src/util';
+
+describe('util', () => {
+  describe('mapObject', () => {
+    it('maps props correctly', () => {
+      const src = {
+        a: 'a',
+        b: 'b',
+        c: 'c',
+      };
+
+      const destTest = {
+        a: 'A',
+        b: 'B',
+        c: 'C',
+      };
+
+      const dest = mapObject(src, (key, obj) => obj[key].toUpperCase());
+
+      expect(dest).toEqual(destTest);
+    });
+  });
+});
