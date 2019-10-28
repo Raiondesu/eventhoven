1623726413e439a91226a065deaf834ba0654bfc
Raiondesu <a.raiondesu@gmail.com>
2019-10-28 16:27:09 +0300

    Update dist

--- dist/js/collections.js
+++ dist/js/collections.js
@@ -4,10 +4,10 @@ var util_1 = require("./util");
 var emit_1 = require("./emit");
 var subscribe_1 = require("./subscribe");
 var unsubscribe_1 = require("./unsubscribe");
-exports.createCollection = function (action) { return function (eventMap) { return util_1.mapObject(eventMap, action(eventMap)); }; };
-exports.emitCollection = exports.createCollection(emit_1.emit);
-exports.subscribeCollection = exports.createCollection(subscribe_1.subscribe);
-exports.unsubscribeCollection = exports.createCollection(unsubscribe_1.unsubscribe);
+var createCollection = function (action) { return function (eventMap) { return util_1.mapObject(eventMap, action(eventMap)); }; };
+exports.emitCollection = createCollection(emit_1.emit);
+exports.subscribeCollection = createCollection(subscribe_1.subscribe);
+exports.unsubscribeCollection = createCollection(unsubscribe_1.unsubscribe);
 exports.eventCollection = function (eventMap) { return ({
     emit: exports.emitCollection(eventMap),
     subscribe: exports.subscribeCollection(eventMap),
