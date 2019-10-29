ee987c8b21cde3aec0d92b0c1ba515d56c702efb
Raiondesu <a.raiondesu@gmail.com>
2019-10-29 17:52:32 +0300

    Update dist

--- dist/js/debug.js
+++ dist/js/debug.js
@@ -5,7 +5,7 @@ const subscribe_1 = require("./subscribe");
 const unsubscribe_1 = require("./unsubscribe");
 const onMeta = subscribe_1.onAll(meta_events_1.metaEvents);
 const offMeta = unsubscribe_1.offAll(meta_events_1.metaEvents);
-const log = ({ event }, _map, eventName, argsOrHandler) => console.log(`${new Date().toJSON().match(/T(.*?)Z/)[1]} [${event.toUpperCase()} "${String(eventName)}"] - `, ...(Array.isArray(argsOrHandler)
+const log = ({ event }, _map, eventName, argsOrHandler) => console.log(`${new Date().toJSON().match(/T\d\d:(.*?)Z/)[1]} [${event.toUpperCase()} "${String(eventName)}"] -`, ...(Array.isArray(argsOrHandler)
     ? argsOrHandler
     : [argsOrHandler]));
 exports.debug = ({ enable, log: logEvent }) => (enable ? onMeta : offMeta)(logEvent || log);
