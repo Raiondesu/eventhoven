790d1639a4b7f75f1bc36bb49c499274f6dc476b
Raiondesu <a.raiondesu@gmail.com>
2019-10-29 17:49:14 +0300

    Update dist

--- dist/es/debug.js
+++ dist/es/debug.js
@@ -3,8 +3,8 @@ import { onAll } from "./subscribe.js";
 import { offAll } from "./unsubscribe.js";
 const onMeta = onAll(metaEvents);
 const offMeta = offAll(metaEvents);
-const log = ({ event }, _map, eventName, argsOrHandler) => console.log(`${new Date().toISOString().match(/T(.*?)Z/)[1]} [EVENT ${event.toUpperCase()} "${String(eventName)}"] - ${Array.isArray(argsOrHandler)
-    ? argsOrHandler.join(', ')
-    : argsOrHandler}`);
+const log = ({ event }, _map, eventName, argsOrHandler) => console.log(`${new Date().toJSON().match(/T(.*?)Z/)[1]} [${event.toUpperCase()} "${String(eventName)}"] - `, ...(Array.isArray(argsOrHandler)
+    ? argsOrHandler
+    : [argsOrHandler]));
 export const debug = ({ enable, log: logEvent }) => (enable ? onMeta : offMeta)(logEvent || log);
 //# sourceMappingURL=debug.js.map
--- dist/js/debug.js
+++ dist/js/debug.js
@@ -5,8 +5,8 @@ const subscribe_1 = require("./subscribe");
 const unsubscribe_1 = require("./unsubscribe");
 const onMeta = subscribe_1.onAll(meta_events_1.metaEvents);
 const offMeta = unsubscribe_1.offAll(meta_events_1.metaEvents);
-const log = ({ event }, _map, eventName, argsOrHandler) => console.log(`${new Date().toISOString().match(/T(.*?)Z/)[1]} [EVENT ${event.toUpperCase()} "${String(eventName)}"] - ${Array.isArray(argsOrHandler)
-    ? argsOrHandler.join(', ')
-    : argsOrHandler}`);
+const log = ({ event }, _map, eventName, argsOrHandler) => console.log(`${new Date().toJSON().match(/T(.*?)Z/)[1]} [${event.toUpperCase()} "${String(eventName)}"] - `, ...(Array.isArray(argsOrHandler)
+    ? argsOrHandler
+    : [argsOrHandler]));
 exports.debug = ({ enable, log: logEvent }) => (enable ? onMeta : offMeta)(logEvent || log);
 //# sourceMappingURL=debug.js.map
