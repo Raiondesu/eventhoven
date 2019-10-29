88cc7035828f3799cf6d6c7c25da202932340d3a
Raiondesu <a.raiondesu@gmail.com>
2019-10-29 22:57:55 +0300

    Update dist

--- dist/es/subscribe.js
+++ dist/es/subscribe.js
@@ -1,14 +1,9 @@
 import { unsubscribe } from "./unsubscribe.js";
 import { emitMeta } from "./emit.js";
 import { doForAll } from "./util.js";
-export const subscribe = (eventMap) => (event) => (...handlers) => {
-    const unsub = (_handlers) => () => unsubscribe(eventMap)(event)(..._handlers);
-    handlers.forEach(handler => {
-        emitMeta('subscribe')(eventMap, event, handler);
-        eventMap[event].set(handler, unsub([handler]));
-    });
-    return unsub(handlers);
-};
+export const subscribe = (eventMap) => (event) => (...handlers) => ((unsub) => unsub(...handlers.map(handler => (emitMeta('subscribe')(eventMap, event, handler),
+    eventMap[event].set(handler, unsub(handler)),
+    handler))))((...handlers) => () => unsubscribe(eventMap)(event)(...handlers));
 export const on = subscribe;
 export const once = (handler) => (_, ...args) => (handler(_, ...args), _.unsubscribe());
 export const subscribeToAll = doForAll(subscribe);
--- dist/js/subscribe.js
+++ dist/js/subscribe.js
@@ -3,14 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const unsubscribe_1 = require("./unsubscribe");
 const emit_1 = require("./emit");
 const util_1 = require("./util");
-exports.subscribe = (eventMap) => (event) => (...handlers) => {
-    const unsub = (_handlers) => () => unsubscribe_1.unsubscribe(eventMap)(event)(..._handlers);
-    handlers.forEach(handler => {
-        emit_1.emitMeta('subscribe')(eventMap, event, handler);
-        eventMap[event].set(handler, unsub([handler]));
-    });
-    return unsub(handlers);
-};
+exports.subscribe = (eventMap) => (event) => (...handlers) => ((unsub) => unsub(...handlers.map(handler => (emit_1.emitMeta('subscribe')(eventMap, event, handler),
+    eventMap[event].set(handler, unsub(handler)),
+    handler))))((...handlers) => () => unsubscribe_1.unsubscribe(eventMap)(event)(...handlers));
 exports.on = exports.subscribe;
 exports.once = (handler) => (_, ...args) => (handler(_, ...args), _.unsubscribe());
 exports.subscribeToAll = util_1.doForAll(exports.subscribe);
