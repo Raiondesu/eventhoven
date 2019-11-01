aa69af8d35566ce95085936ea758635943e75925
Raiondesu <a.raiondesu@gmail.com>
2019-11-01 16:41:35 +0300

    Fix [for-loop] src/unsubscribe - fix `if` positioning
    `if` inside of the loop doesn't provide a sufficient protection

--- src/unsubscribe.ts
+++ src/unsubscribe.ts
@@ -10,11 +10,11 @@ export const unsubscribe = <M extends TEventMap>(
 ): TUnsubscribeHandlers<M, E> => (
   ...handlers
 ) => {
-  for (
+  if (event in eventMap) for (
     const h of handlers.length > 0
       ? handlers
       : eventMap[event].keys()
-  ) if (event in eventMap)
+  )
     // Emit meta-event (ignore promise)
     emitMeta(EMetaEvents.UNSUBSCRIBE)(eventMap, event, h),
     eventMap[event].delete(h);
