013c1d173dd1254d61f1ff3618e65aa7e3d749ed
Raiondesu <a.raiondesu@gmail.com>
2019-11-01 16:42:31 +0300

    Update dist

--- dist/esnext/unsubscribe.js
+++ dist/esnext/unsubscribe.js
@@ -1,10 +1,10 @@
 import { emitMeta } from "./emit.js";
 import { doForAll } from "./util.js";
 export const unsubscribe = (eventMap) => (event) => (...handlers) => {
-    for (const h of handlers.length > 0
-        ? handlers
-        : eventMap[event].keys())
-        if (event in eventMap)
+    if (event in eventMap)
+        for (const h of handlers.length > 0
+            ? handlers
+            : eventMap[event].keys())
             emitMeta("UNSUBSCRIBE")(eventMap, event, h),
                 eventMap[event].delete(h);
 };
--- dist/js/unsubscribe.js
+++ dist/js/unsubscribe.js
@@ -3,10 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const emit_1 = require("./emit");
 const util_1 = require("./util");
 exports.unsubscribe = (eventMap) => (event) => (...handlers) => {
-    for (const h of handlers.length > 0
-        ? handlers
-        : eventMap[event].keys())
-        if (event in eventMap)
+    if (event in eventMap)
+        for (const h of handlers.length > 0
+            ? handlers
+            : eventMap[event].keys())
             emit_1.emitMeta("UNSUBSCRIBE")(eventMap, event, h),
                 eventMap[event].delete(h);
 };
