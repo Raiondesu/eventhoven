4db3d66e174515cfd971216667412c6a96c4d24b
Raiondesu <a.raiondesu@gmail.com>
2019-11-02 13:23:03 +0300

    Fix [call order] src/once - call unsubscribe before the handler
    - to avoid unexpected async calls to the handler

--- src/once.ts
+++ src/once.ts
@@ -1,9 +1,19 @@
 import { TEventMap, THandlerOf, TEventContext, TLastParams } from './types';
 
+/**
+ * Makes a handler to be called only once,
+ * by unsubscribing it right at its execution
+ *
+ * @param handler - a handler to transform
+ *
+ * @returns a transformed handler
+ */
 export const once = <
   M extends TEventMap,
   E extends keyof M
 >(handler: THandlerOf<M, E>) => (
   _: TEventContext<M>,
   ...args: TLastParams<THandlerOf<M, E>>
-) => (handler(_, ...args), _.unsubscribe());
+) => <
+  ReturnType<THandlerOf<M, E>>
+> (_.unsubscribe(), handler(_, ...args));
