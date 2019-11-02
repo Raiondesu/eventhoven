085cc7849340592857d768419ec5790decc84beb
Raiondesu <a.raiondesu@gmail.com>
2019-11-02 19:15:39 +0300

    Update dist

--- dist/esnext/subscribe.js
+++ dist/esnext/subscribe.js
@@ -1,8 +1,8 @@
 import { unsubscribe } from "./unsubscribe.js";
 import { emitMeta } from "./emit.js";
-export const subscribe = (eventMap) => (event) => (...handlers) => {
+export const subscribe = (eventMap) => (event) => {
     const unsub = (...handlers) => () => unsubscribe(eventMap)(event)(...handlers);
-    return unsub(...handlers.map(handler => (emitMeta("SUBSCRIBE")(eventMap, event, handler),
+    return (...handlers) => unsub(...handlers.map(handler => (emitMeta("SUBSCRIBE")(eventMap, event, handler),
         event in eventMap && eventMap[event].set(handler, unsub(handler)),
         handler)));
 };
--- dist/js/subscribe.js
+++ dist/js/subscribe.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const unsubscribe_1 = require("./unsubscribe");
 const emit_1 = require("./emit");
-exports.subscribe = (eventMap) => (event) => (...handlers) => {
+exports.subscribe = (eventMap) => (event) => {
     const unsub = (...handlers) => () => unsubscribe_1.unsubscribe(eventMap)(event)(...handlers);
-    return unsub(...handlers.map(handler => (emit_1.emitMeta("SUBSCRIBE")(eventMap, event, handler),
+    return (...handlers) => unsub(...handlers.map(handler => (emit_1.emitMeta("SUBSCRIBE")(eventMap, event, handler),
         event in eventMap && eventMap[event].set(handler, unsub(handler)),
         handler)));
 };
