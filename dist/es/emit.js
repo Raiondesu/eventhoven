eb7dc820e9260995673203f77ed5f24347974f12
Raiondesu <a.raiondesu@gmail.com>
2019-11-05 13:18:15 +0300

    Update dist

--- dist/es/emit.js
+++ dist/es/emit.js
@@ -1,9 +1,9 @@
 import { metaEvents } from "./meta-events.js";
-export const emit = (eventMap) => (event) => (...args) => new Promise((resolve, e) => Promise.all([
+export const emit = (eventMap) => (event) => (...args) => Promise.all([
     emitMeta("EMIT")(eventMap, event, args),
     ...[...(eventMap[event] || [])].map(([handler, unsubscribe]) => handler
         && handler({ event, unsubscribe }, ...args))
-]).then(_ => resolve(), e));
+]).then(_ => void 0);
 export const emitMeta = (event) => (...args) => args[0] !== metaEvents
     ? emit(metaEvents)(event)(...args)
     : Promise.resolve();
