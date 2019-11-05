3eb6b88b830623692b145ec87feccd0690d32d3f
Raiondesu <a.raiondesu@gmail.com>
2019-11-05 13:35:41 +0300

    Update dist

--- dist/es/debug.js
+++ dist/es/debug.js
@@ -2,8 +2,6 @@ import { metaEvents } from "./meta-events.js";
 import { onAll, offAll } from "./all.js";
 const onMeta = onAll(metaEvents);
 const offMeta = offAll(metaEvents);
-const log = ({ event }, map, eventName, argsOrHandler) => console.log(new Date().toJSON().substr(14, 9), `[${event} "${String(eventName)}"${eventName in map ? '' : ' (INVALID)'}] -`, ...(Array.isArray(argsOrHandler)
-    ? argsOrHandler
-    : [argsOrHandler]));
+const log = ({ event }, map, eventName, argsOrHandler) => console.log(new Date().toJSON().substr(14, 9), `[${event} "${String(eventName)}"${eventName in map ? '' : ' (INVALID)'}] -`, argsOrHandler);
 export const debug = ({ enable, log: logEvent = log }) => (enable ? onMeta : offMeta)(logEvent);
 //# sourceMappingURL=debug.js.map
--- dist/js/debug.js
+++ dist/js/debug.js
@@ -4,8 +4,6 @@ const meta_events_1 = require("./meta-events");
 const all_1 = require("./all");
 const onMeta = all_1.onAll(meta_events_1.metaEvents);
 const offMeta = all_1.offAll(meta_events_1.metaEvents);
-const log = ({ event }, map, eventName, argsOrHandler) => console.log(new Date().toJSON().substr(14, 9), `[${event} "${String(eventName)}"${eventName in map ? '' : ' (INVALID)'}] -`, ...(Array.isArray(argsOrHandler)
-    ? argsOrHandler
-    : [argsOrHandler]));
+const log = ({ event }, map, eventName, argsOrHandler) => console.log(new Date().toJSON().substr(14, 9), `[${event} "${String(eventName)}"${eventName in map ? '' : ' (INVALID)'}] -`, argsOrHandler);
 exports.debug = ({ enable, log: logEvent = log }) => (enable ? onMeta : offMeta)(logEvent);
 //# sourceMappingURL=debug.js.map
