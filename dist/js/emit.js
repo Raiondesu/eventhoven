7548c1967400cd7d4541b60ebf28607b0f38c55f
Raiondesu <a.raiondesu@gmail.com>
2019-11-08 11:33:21 +0300

    Update dist

--- dist/js/emit.js
+++ dist/js/emit.js
@@ -1,12 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const meta_events_1 = require("./meta-events");
-exports.emit = (eventMap) => (event) => (...args) => Promise.all([
-    exports.emitMeta("EMIT")(eventMap, event, args),
-    ...[...(eventMap[event] || [])].map(([handler, unsubscribe]) => handler
-        && handler({ event, unsubscribe }, ...args))
-]).then(_ => void 0);
+exports.emit = (eventMap) => (event) => (...args) => exports.emitMeta("EMIT")(eventMap, event, args).then(_ => Promise.all([...(eventMap[event] || [])].map(([handler, unsubscribe]) => handler
+    && handler({ event, unsubscribe }, ...args))));
 exports.emitMeta = (event) => (...args) => args[0] !== meta_events_1.metaEvents
     ? exports.emit(meta_events_1.metaEvents)(event)(...args)
-    : Promise.resolve();
+    : Promise.resolve([]);
 //# sourceMappingURL=emit.js.map
