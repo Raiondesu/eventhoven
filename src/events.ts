d0386b683faee4a948467069a76638f760797661
Raiondesu <a.raiondesu@gmail.com>
2019-11-11 00:39:55 +0300

    Update [jsdoc] src/events - disclaimer

--- src/events.ts
+++ src/events.ts
@@ -15,6 +15,6 @@ export const eventMap = <Events extends TEventSignatures>(
   // This might be due to the initialization of iterators in the constructor (https://tc39.es/ecma262/#sec-map-iterable),
   // whereas no iterators are used in the `set` method (https://tc39.es/ecma262/#sec-map.prototype.set).
   event => new Map().set(events[event], () => {
-    /* make it impossible to unsubscribe from the default handler */
+    /* make it impossible to unsubscribe from within the default handler */
   })
 );
