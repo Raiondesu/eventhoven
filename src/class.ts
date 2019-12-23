f66775206fdc0bf7d77c7ee53d9c6b69cfa306cb
Raiondesu <a.raiondesu@gmail.com>
2019-12-23 18:00:41 +0300

    Revert "Fix [code duplication] class - hopefully"
    This reverts commit 341e0492aea0cee53fe7ff5089d917ded0a99b8d.

--- src/class.ts
+++ src/class.ts
@@ -14,21 +14,25 @@ export class Eventhoven<
     this.map = eventMap(events) as M;
   }
 
-  private apply<F extends (arg1: any) => (arg2: any) => (...args: any[]) => any>(action: F) {
-    return (arg, ...args) => action(this.map)(arg)(...args);
+  private apply<F extends (arg1: any) => (arg2: any) => (...args: any[]) => any>(
+    action: F,
+    arg2: any,
+    args: any[]
+  ) {
+    return action(this.map)(arg2)(...args);
   }
 
-  public readonly emit: {
-    <E extends keyof T>(event: E, ...args: TLastParams<THandlerOf<M, E>>): TEmitReturn<M ,E>;
-   } = this.apply(emit);
+  public emit<E extends keyof T>(event: E, ...args: TLastParams<THandlerOf<M, E>>): TEmitReturn<M ,E> {
+    return this.apply(emit, event, args);
+  }
 
-  public readonly on: {
-    <E extends keyof T>(event: E, ...handlers: Array<THandlerOf<M, E>>): TSubscriber<M, E>;
-  } = this.apply(on);
+  public on<E extends keyof T>(event: E, ...handlers: Array<THandlerOf<M, E>>): TSubscriber<M, E> {
+    return this.apply(on, event, handlers);
+  }
 
-  public readonly off: {
-    <E extends keyof T>(event: E, ...handlers: Array<THandlerOf<M, E>>): void;
-  } = this.apply(off);
+  public off<E extends keyof T>(event: E, ...handlers: Array<THandlerOf<M, E>>): void {
+    return this.apply(off, event, handlers);
+  }
 
   public static readonly emit = emit;
   public static readonly on = on;
