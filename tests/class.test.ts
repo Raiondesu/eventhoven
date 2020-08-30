da41cf807d690f9e82131bf9e7bf1661178f9b05
Alexey Iskhakov <a.raiondesu@gmail.com>
2020-08-30 20:10:51 +0300

    Add test for new static methods in Class API

--- tests/class.test.ts
+++ tests/class.test.ts
@@ -1,4 +1,4 @@
-import { test_eventSignatures } from './common';
+import { test_eventSignatures, test_eventMap } from './common';
 import { Eventhoven } from '../src';
 
 describe('Eventhoven', () => {
@@ -23,6 +23,7 @@ describe('Eventhoven', () => {
 
   it('proxies events properly', async () => {
     const handler = jest.fn();
+    const handler2 = jest.fn();
     const eventh = new Eventhoven(test_eventSignatures);
 
     await eventh.emit('event3');
@@ -33,10 +34,43 @@ describe('Eventhoven', () => {
     await eventh.emit('event3');
 
     expect(handler).toHaveBeenCalledTimes(1);
+    expect(handler2).not.toHaveBeenCalled();
 
-    eventh.off('event3', handler);
+    // If we want to re-assign another handler to an event - just pipe the calls
+    eventh
+      .off('event3', handler)
+      .on('event3', handler2);
     await eventh.emit('event3');
 
+    expect(handler2).toHaveBeenCalledTimes(1);
     expect(handler).toHaveBeenCalledTimes(1);
+  });
+
+  it('correctly flattens FP API in static methods', () => {
+    const handlerClass = jest.fn();
+    const handlerMap = jest.fn();
+    const test_eventClass = new Eventhoven(test_eventSignatures);
+
+    Eventhoven.on(test_eventClass, 'event3', handlerClass);
+    Eventhoven.emit(test_eventClass, 'event3'/* , no third argument since this event has no parameters */);
+
+    expect(handlerClass).toHaveBeenCalledTimes(1);
+
+    Eventhoven.off(test_eventClass, 'event3', handlerClass);
+    Eventhoven.emit(test_eventClass, 'event3'/* , no third argument since this event has no parameters */);
+
+    // Since the `off` method should have worked, the handler shouldn't have been called
+    expect(handlerClass).toHaveBeenCalledTimes(1);
+
+
+    Eventhoven.on(test_eventMap, 'event3', handlerMap);
+    Eventhoven.emit(test_eventMap, 'event3'/* , no third argument since this event has no parameters */);
+
+    expect(handlerMap).toHaveBeenCalledTimes(1);
+
+    Eventhoven.off(test_eventMap, 'event3', handlerMap);
+    Eventhoven.emit(test_eventMap, 'event3'/* , no third argument since this event has no parameters */);
+
+    expect(handlerMap).toHaveBeenCalledTimes(1);
   });
 });
